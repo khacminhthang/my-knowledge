2acb4df59215da73e1302c52213066e2d8e56a3a
thanglkm <thanglkm@vnpt.vn>
2024-08-21 14:40:06 +0700

    @

--- src/app/components/micro-fe/module-federation/manage-library-versions-with-module-federation/manage-library-versions-with-module-federation.component.ts
+++ src/app/components/micro-fe/module-federation/manage-library-versions-with-module-federation/manage-library-versions-with-module-federation.component.ts
@@ -7,6 +7,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageLibraryVersionsWithModuleFederationComponent implements OnInit {
 
+  text1 = `
+  import { ModuleFederationConfig } from '@nx/webpack';
+
+  const config: ModuleFederationConfig = {
+    name: 'remote',
+    exposes: {
+      './Module': './src/remote-entry.ts',
+    },
+    // Determine which libraries to share
+    shared: (packageName: string) {
+      // I do not want to share this package and I will load my own version
+      if(packageName === '@acme/utils') return false;
+    }
+  };
+  export default config;
+  `
+  text2 = `
+  module.exports = {
+    plugins: [
+      new ModuleFederationPlugin({
+        // additional config
+        name: 'remote',
+        shared: {
+          react: { singleton: true, eager: true },
+          // acme/utils will not be shared
+        },
+      }),
+    ],
+  };
+  `
+  text3 = `
+  import { ModuleFederationConfig } from '@nx/webpack';
+
+  const config: ModuleFederationConfig = {
+    name: 'remote',
+    exposes: {
+      './Module': './src/remote-entry.ts',
+    },
+    // By not declaring a shared function, all dependencies will be shared
+  };
+  export default config;
+  `
+  text4 = `
+    module.exports = {
+    // Additional config ignored for brevity
+    plugins: [
+      new ModuleFederationPlugin({
+        // ...
+        name: 'remote',
+        shared: {
+          react: { singleton: true, eager: true, version: '18.2.0' },
+          'acme/utils': { singleton: true, eager: true, version: '1.0.0' }, // <--- This version is determined by the logic discussed earlier
+        },
+      }),
+    ],
+  };
+  `
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/components/micro-fe/module-federation/micro-frontend-architecture/micro-frontend-architecture.component.ts
+++ src/app/components/micro-fe/module-federation/micro-frontend-architecture/micro-frontend-architecture.component.ts
@@ -7,6 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MicroFrontendArchitectureComponent implements OnInit {
 
+  text1 = `
+  nx g @nx/angular:host shell --remotes=shop,cart
+  nx g @nx/angular:remote about --host=shell
+  `
+  text2 = `
+  import { ModuleFederationConfig } from '@nx/webpack';
+  // Core libraries such as react, angular, redux, ngrx, etc. must be
+  // singletons. Otherwise the applications will not work together.
+  const coreLibraries = new Set([
+    'react',
+    'react-dom',
+    'react-router-dom',
+    // A workspace library for a publish/subscribe
+    // system of communication.
+    '@acme/pub-sub',
+  ]);
+
+  export const config: ModuleFederationConfig = {
+    // Share core libraries, and avoid everything else
+    shared: (libraryName, defaultConfig) => {
+      if (coreLibraries.has(libraryName)) {
+        return defaultConfig;
+      }
+
+      // Returning false means the library is not shared.
+      return false;
+    },
+  };
+
+  export default config;
+  `
+  text3 = `
+  import { ModuleFederationConfig } from '@nx/webpack';
+  import baseConfig from '../../module-federation.config';
+
+  export const config: ModuleFederationConfig = {
+    ...baseConfig,
+    name: 'shell',
+    remotes: ['shop', 'cart', 'about'],
+  };
+
+  export default config;
+  `
   constructor() { }
 
   ngOnInit(): void {
